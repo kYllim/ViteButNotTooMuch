931d430576af31f9437e9af571f8195d03eb1864
kYllim <130597619+kYllim@users.noreply.github.com>
2025-06-23 08:08:18 +0200

    ajouts 22/06/2025

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" loading="lazy" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${javascriptLogo}" class="logo vanilla" loading="lazy" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
@@ -19,6 +19,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
